166a4552ebc26ece6d094dea1438002c5e142aaf
erfunty <er12fa34nty56@gmail.com>
2026-09-19 23:28:42 +0330

    day 1 complete (print input var)

---
+++ days01-10/days01/Exe4-variables.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code above 👇
+a=input("a:")
+b=input("b:")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+t=a
+a=b
+b=t
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a2:"+a)
+print("b2:"+b)
